41ed268d48779bf89ec4df6725a8431318fb9e86
dencelman1 <dencelman@gmail.com>
2024-02-06 23:38:02 +0200

    added library api objects

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import AnyBackAdminPanel from "./AnyBackAdminPanel";
+import { useAdminPanel } from "./hooks/useAdminPanel";
+import useAdminSection from "./hooks/useAdminSection";
+
+
+export {
+    useAdminPanel,
+    useAdminSection,
+    AnyBackAdminPanel,
+}
